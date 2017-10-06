f54a5a982e0f11af2232735bc5f80f610368f137
Daniel Silva <danny@dannyps.net>
2017-10-06 17:55:57 +0000

    Testing set frequency

--- lab2/timer.c
+++ lab2/timer.c
@@ -135,8 +135,8 @@ int timer_set_frequency(unsigned char timer_conf, unsigned long freq) {
 
 	long a=255;
 	printf("%x\n%x\n%x\n", clock, clock/255, (clock & a));
-	sys_outb(TIMER_0, clock/255);
 	sys_outb(TIMER_0, clock%255);
+	sys_outb(TIMER_0, clock/255);
 
 	return 1;
 }
