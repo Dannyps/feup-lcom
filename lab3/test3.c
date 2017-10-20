63c928473113ef23d36f041fd019eb25d5e77d21
Daniel Silva <danny@dannyps.net>
2017-10-20 16:06:14 +0000

    Create lab3.c based on lab2.c.

--- lab3/test3.c
+++ lab3/test3.c
@@ -1,9 +1,13 @@
 int kbd_test_scan(unsigned short assembly) {
+
 	/* To be completed */
+	return 0;
 }
 int kbd_test_poll() {
     /* To be completed */
+	return 0;
 }
 int kbd_test_timed_scan(unsigned short n) {
     /* To be completed */
+	return 0;
 }
