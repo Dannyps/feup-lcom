11a95f8cd21834018c5f3dfab236dfe6b712ebb2
up201408030 <up201408030@0416bdf5-df4b-41b9-a32b-9c14e5908e50>
2017-10-26 23:03:10 +0000

    Haven't tested yet, but kbd_test_timed_scan() should be working!

--- lab3/test3.c
+++ lab3/test3.c
@@ -36,6 +36,9 @@ int sys_inb_cnt(port_t port, unsigned long* byte){
 int timer0_hookIDs[2];
 int kbc_hookIDs[2];
 
+int seconds_elapsed=0;
+int tick_elapsed=0;
+
 char stop;
 
 
@@ -64,6 +67,25 @@ int timer0_subscribe_int(void ) {
 
 	return ret;
 }
+
+int timer0_unsubscribe_int() {
+
+        return sys_irqrmpolicy(&timer0_hookIDs[1]);
+
+}
+
+void timer0_int_handler() {
+
+        if((++tick_elapsed)%60==0){
+
+                printf("1 sec! %d\n", ++time_elapsed);
+
+        }
+
+        return;
+
+}
+
 
 
 int kbd_unsubscribe_int() {
@@ -129,7 +151,7 @@ int kbd_test_scan_c(){
 
 	if(kbd_unsubscribe_int()!=0){
 		fprintf(stderr, "Could not unsubscribe from IRQ_0.\n");
-		exit(-9);
+		exit(-2);
 	}
 	printf("\nunsubscribed successfully.\n");
 
@@ -177,6 +199,64 @@ int kbd_test_poll() {
 
 int kbd_test_timed_scan(unsigned short n) {
     /* To be completed */
+	
+	/* Subscribes to timer */
+	int timer0_ret = timer0_subscribe_int();
+    if(timer0_ret!=0){
+        fprintf(stderr, "Couldn't subscribe to IRQ_0!\n");exit(-3);
+    }
+
+	/* Subscribes to kbd */
+	int ret = kbd_subscribe_int();
+	if(ret!=0){
+		fprintf(stderr, "Could not subscribe interruptions for the kbc!\n");exit(-4);
+	}
+
+	int ipc_status;
+	int r;
+	message msg;
+	stop=0;
+	while(!stop) { /*	You may want to use a different condition*/
+		/*Get a request message.*/
+		 if( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 ) {
+			 printf("driver_receive failed with: %d", r);
+			 continue;
+		 }
+		 if (is_ipc_notify(ipc_status)) {
+			 int irq_set=0; irq_set |= BIT(0); // 0 as in the kbc_hookIDs[0].
+			 switch (_ENDPOINT_P(msg.m_source)) {
+				 case HARDWARE:
+					if (msg.NOTIFY_ARG & irq_timer0) {
+						timer0_int_handler();
+					}
+					if (msg.NOTIFY_ARG & irq_set) {
+						kbd_int_handler();
+					}
+				 break;
+			 default:
+				 break;
+					 //no other notifications expected: do nothing
+			 }
+		 } else { /*received a standard message, not a notification*/
+			 /*no standard messages expected: do nothing*/
+		 }
+		
+		//if(time_elapsed>=time){break;}
+		if(seconds_elapsed >= n)
+			{break;}
+	}
+
+	if(timer_unsubscribe_int()!=0){
+        fprintf(stderr, "Could not unsubscribe from IRQ_0.\n"); exit(-5);
+    }
+
+	
+	if(kbd_unsubscribe_int()!=0){
+		fprintf(stderr, "Could not unsubscribe from IRQ_0.\n");	exit(-6);
+	}
+	
+	printf("\nunsubscribed successfully.\n");
+
 	return 0;
 }
 
