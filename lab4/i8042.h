ef987873e5bb17e210dcc082cb52e97e487fc443
up201503212 <up201503212@0416bdf5-df4b-41b9-a32b-9c14e5908e50>
2017-11-08 16:08:09 +0000

    Write wtr2Mouse and begin work on mouse_test_packet

---
+++ lab4/i8042.h
@@ -0,0 +1,43 @@
+#ifndef _LCOM_I8042_H_
+#define _LCOM_I8042_H_
+
+typedef unsigned char uchar;
+
+/** @defgroup i8042 i8042
+ * @{
+ *
+ * Constants for programming the i8042 KBC. Needs to be completed.
+ */
+
+
+#define BIT(n) (0x01<<(n))
+
+#define KBC_IRQ	        1    	/**< @brief KBC IRQ line */
+#define MOUSE_IRQ		12    	/**< @brief Mouse IRQ line */
+
+/* I/O port addresses */
+
+#define OUT_BUF			0x60 	/**< @brief kbc output buffer */
+#define IN_BUF			0x60 	/**< @brief kbc input buffer */
+
+#define STATUS_REG		0x64 	/**< @brief kbc input buffer */
+#define CTRL_REG		0x64 	/**< @brief kbc control register */
+
+/* KBC COMMANDS */
+#define WRITE_BYTE		0xD4	/**< @brief kbc WRITE BYTE command, called b4 mouse commands */
+
+/* MOUSE COMMANDS */
+
+#define ENABLE_STREAM_MODE	0xF4	/**< @brief mouse: enable stream mode */
+#define DISABLE_STREAM_MODE	0xF5	/**< @brief mouse: disable stream mode */
+
+/* Mouse controller -- Acknowledgment bytes */
+
+#define MOUSE_ACK		0xFA	/**< @brief mouse response: everything is ok */
+#define MOUSE_NACK		0xFE	/**< @brief mouse response: invalid byte */
+#define MOUSE_ERROR		0xFC	/**< @brief mouse response: second consecutive invalid byte */
+
+/**@}*/
+
+#endif /* _LCOM_I8254_H */
+
