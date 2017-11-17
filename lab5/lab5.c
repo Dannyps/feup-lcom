21db57c3084602fe1e89c4919db50a3ae997ae7a
Daniel Silva <danny@dannyps.net>
2017-11-17 14:18:33 +0000

    Import lab5

---
+++ lab5/lab5.c
@@ -0,0 +1,240 @@
+#include <limits.h>
+#include <string.h>
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <minix/drivers.h>
+
+#include "test5.h"
+
+static int proc_args(int argc, char **argv);
+static unsigned long parse_ulong(char *str, int base);
+static void print_usage(char **argv);
+
+
+void exit_handler(){
+	printf("--- FINISHED\n");
+	return;
+}
+
+int main(int argc, char **argv)
+{
+	/* DO NOT FORGET TO initialize service */
+	sef_startup();
+	atexit(exit_handler);
+	printf("\n--- STARTED\n");
+
+	fflush(stdout);
+
+
+	if (argc == 1) {					/* Prints usage of the program if no arguments are passed */
+		print_usage(argv);
+		return 0;
+	}
+	else return proc_args(argc, argv);
+}
+
+static void print_usage(char **argv)
+{
+	printf("Usage: one of the following:\n"
+			"\t service run %s -args \"init <mode> <delay>\"\n"
+			"\t service run %s -args \"square <x> <y> <size> <color>\"\n"
+			"\t service run %s -args \"line <xi> <yi> <xf> <yf> <color>\"\n"
+			"\t service run %s -args \"xpm <xpmID> <x> <y>\"\n"
+			"\t service run %s -args \"move <xpmID> <xi> <yi> <xf> <yf> <speed> <fr>\"\n"
+			"\t service run %s -args \"controller\"\n",
+			argv[0], argv[0], argv[0], argv[0], argv[0], argv[0]);
+}
+
+
+/* GCP <= Graphics Card Program */
+
+static int proc_args(int argc, char **argv)
+{
+	if (strncmp(argv[1], "init", strlen("init")) == 0) {
+		unsigned long mode, delay;
+		if (argc != 2+2) {
+			printf("GCP: wrong no. of arguments for video_test_init()\n");
+			return 1;
+		}
+
+		delay = parse_ulong(argv[3], 10);						/* Parses string to unsigned long */
+		if (delay == ULONG_MAX)
+			return 1;
+
+		int ret=sscanf(argv[2], "0x%x", &mode);
+		if(ret != 1){
+			printf("GCP: mode must be specified in the following format: 0x105\n");
+			return 1;
+		}
+
+		printf("GCP::video_test_init(0x%x, %lu)\n", mode, delay);
+		video_test_init(mode, delay);
+		return 0;
+	}
+	else if (strncmp(argv[1], "square", strlen("square")) == 0) {
+		unsigned long x, y, size, color;
+		if (argc != 2+4) {
+			printf("GCP: wrong no. of arguments for video_test_square()\n");
+			return 1;
+		}
+
+		x = parse_ulong(argv[2], 10);						/* Parses string to unsigned long */
+		if (x == ULONG_MAX)
+			return 1;
+
+		y = parse_ulong(argv[3], 10);						/* Parses string to unsigned long */
+		if (y == ULONG_MAX)
+			return 1;
+
+		size = parse_ulong(argv[4], 10);					/* Parses string to unsigned long */
+		if (size == ULONG_MAX)
+			return 1;
+
+		int ret=sscanf(argv[5], "0x%x", &color);
+		if(ret != 1){
+			printf("GCP: color must be specified in the following format: 0x105\n");
+			return 1;
+		}
+
+		printf("GCP::video_test_square(%lu, %lu, %lu, 0x%x)\n", x, y, size, color);
+		return video_test_square(x, y, size, color);
+	}
+	else if (strncmp(argv[1], "line", strlen("line")) == 0) {
+		unsigned long xi, yi, xf, yf, color;
+		if (argc != 2+5) {
+			printf("GCP: wrong no. of arguments for video_test_line()\n");
+			return 1;
+		}
+
+		xi = parse_ulong(argv[2], 10);						/* Parses string to unsigned long */
+		if (xi == ULONG_MAX)
+			return 1;
+
+		yi = parse_ulong(argv[3], 10);						/* Parses string to unsigned long */
+		if (yi == ULONG_MAX)
+			return 1;
+
+		xf = parse_ulong(argv[4], 10);						/* Parses string to unsigned long */
+		if (xi == ULONG_MAX)
+			return 1;
+
+		yf = parse_ulong(argv[5], 10);						/* Parses string to unsigned long */
+		if (yi == ULONG_MAX)
+			return 1;
+
+		color = parse_ulong(argv[6], 10);					/* Parses string to unsigned long */
+		if (color == ULONG_MAX)
+			return 1;
+
+		int ret=sscanf(argv[5], "0x%x", &color);
+		if(ret != 1){
+			printf("GCP: color must be specified in the following format: 0x105\n");
+			return 1;
+		}
+
+		printf("GCP::video_test_line(%lu, %lu, %lu, %lu, 0x%x)\n", xi, yi, xf, yf, color);
+		return video_test_line(xi, yi, xf, yf, color);
+	}
+	else if (strncmp(argv[1], "xpm", strlen("xpm")) == 0) {
+		unsigned long xi, yi, xpm;
+		if (argc != 2+5) {
+			printf("GCP: wrong no. of arguments for test_xpm()\n");
+			return 1;
+		}
+
+		xpm = parse_ulong(argv[2], 10);						/* Parses string to unsigned long */
+		if (xpm == ULONG_MAX)
+			return 1;
+
+		xi = parse_ulong(argv[3], 10);						/* Parses string to unsigned long */
+		if (xi == ULONG_MAX)
+			return 1;
+
+		yi = parse_ulong(argv[4], 10);						/* Parses string to unsigned long */
+		if (yi == ULONG_MAX)
+			return 1;
+
+		printf("GCP::test_xpm(%lu, %lu, %lu)\n", xpm, xi, yi);
+		return test_xpm(0, xi, yi); //TODO take care of that zero
+	}
+	else if (strncmp(argv[1], "move", strlen("move")) == 0) {
+		unsigned long xi, xf, yf, fr, yi, xpm, speed;
+		if (argc != 2+5) {
+			printf("GCP: wrong no. of arguments for test_move()\n");
+			return 1;
+		}
+
+		xpm = parse_ulong(argv[2], 10);						/* Parses string to unsigned long */
+		if (xpm == ULONG_MAX)
+			return 1;
+
+		xi = parse_ulong(argv[3], 10);						/* Parses string to unsigned long */
+		if (xi == ULONG_MAX)
+			return 1;
+
+		yi = parse_ulong(argv[4], 10);						/* Parses string to unsigned long */
+		if (yi == ULONG_MAX)
+			return 1;
+
+		xf = parse_ulong(argv[5], 10);						/* Parses string to unsigned long */
+		if (xf == ULONG_MAX)
+			return 1;
+
+		yf = parse_ulong(argv[6], 10);						/* Parses string to unsigned long */
+		if (yf == ULONG_MAX)
+			return 1;
+
+		speed = parse_ulong(argv[7], 10);					/* Parses string to unsigned long */
+		if (speed == ULONG_MAX)
+			return 1;
+
+		fr = parse_ulong(argv[8], 10);						/* Parses string to unsigned long */
+		if (fr == ULONG_MAX)
+			return 1;
+
+
+		printf("GCP::test_move(%lu, %lu, %lu, %lu, %lu, %lu, %lu)\n", xpm, xi, yi, xf, yf, speed, fr);
+		return test_move(0, xi, yi, xf, yf, speed, fr); //TODO take care of that zero
+	}
+//	else if (strncmp(argv[1], "int", strlen("int")) == 0) {
+//		if (argc != 3) {
+//			printf("timer: wrong no of arguments for timer_test_int()\n");
+//			return 1;
+//		}
+//		time = parse_ulong(argv[2], 10);						/* Parses string to unsigned long */
+//		if (time == ULONG_MAX)
+//			return 1;
+//		printf("timer::timer_test_int(%lu)\n", time);
+//		return timer_test_int(time);
+//	}
+	else {
+		printf("GCP: %s - no valid function!\n", argv[1]);
+		return 1;
+	}
+}
+
+static unsigned long parse_ulong(char *str, int base)
+{
+	char *endptr;
+	unsigned long val;
+
+	/* Convert string to unsigned long */
+	val = strtoul(str, &endptr, base);			
+
+	/* Check for conversion errors */
+	if ((errno == ERANGE && val == ULONG_MAX) || (errno != 0 && val == 0)) {
+		perror("strtoul");
+		return ULONG_MAX;
+	}
+
+	if (endptr == str) {
+		printf("timer: parse_ulong: no digits were found in %s\n", str);
+		return ULONG_MAX;
+	}
+
+	/* Successful conversion */
+	return val;
+}
---
+++ lab5/test5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "vbe.h"
+#include "video_gr.h"
+
+void *video_test_init(unsigned short mode, unsigned short delay) {
+	printf("entered!\n");
+	/* To be completed */
+	return 0;
+}
+
+
+int video_test_square(unsigned short x, unsigned short y, unsigned short size, unsigned long color) {
+	
+	/* To be completed */
+	return 0;
+}
+
+int video_test_line(unsigned short xi, unsigned short yi, 
+		           unsigned short xf, unsigned short yf, unsigned long color) {
+	
+	/* To be completed */
+	return 0;
+}
+	
+int test_xpm(char *xpm[], unsigned short xi, unsigned short yi) {
+	
+	/* To be completed */
+	return 0;
+}	
+
+int test_move(char *xpm[], unsigned short xi, unsigned short yi, unsigned short xf, unsigned short yf, short s, unsigned short f) {
+
+	/* To be completed */
+	return 0;
+}	
+
+int test_controller() {
+
+	/* To be completed */
+	return 0;
+}	
+	
