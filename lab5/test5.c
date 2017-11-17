34d9c9551dabf0a4e42556989c85658b7883a7c4
up201408030 <up201408030@0416bdf5-df4b-41b9-a32b-9c14e5908e50>
2017-11-17 16:58:12 +0000

    implement video_start for use in other functions

--- lab5/test5.c
+++ lab5/test5.c
@@ -8,12 +8,18 @@ void setP(unsigned long x, unsigned long y, unsigned char color){
 	video_m[y*H_RES+x]=color;
 }
 
+
+void video_start(){
+	video_m=vg_init(0x105);
+}
+
 void *video_test_init(unsigned short mode, unsigned short delay) {
 	printf("Initing mode 0x%x!\n", mode);
-	sleep(1);
+
 	video_m=vg_init(mode);
 
 	sleep(delay);
+
 	vg_exit();
 	return 0;
 }
@@ -21,7 +27,8 @@ void *video_test_init(unsigned short mode, unsigned short delay) {
 
 int video_test_square(unsigned short x, unsigned short y, unsigned short size, unsigned long color) {
 	
-	/* To be completed */
+
+
 	return 0;
 }
 
