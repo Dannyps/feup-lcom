2e5590c99675451c05851e748a312e67151c5d90
up201408030 <up201408030@0416bdf5-df4b-41b9-a32b-9c14e5908e50>
2017-12-06 17:34:10 +0000

    Started month view functionality

---
+++ Proj/Src/view.h
@@ -0,0 +1,62 @@
+#ifndef __VIEW_H
+#define __VIEW_H
+
+#include <stdio.h>
+#include <minix/syslib.h>
+#include <minix/driver.h>
+#include <minix/drivers.h>
+#include <minix/com.h>
+#include <machine/int86.h>
+#include <sys/time.h>
+#include <time.h>
+
+#include "vbe.h"
+#include "video_gr.h"
+#include "i8042.h"
+#include "i8254.h"
+#include <minix/sysutil.h>
+#include "lmlib.h"
+
+#include "keyboard.h"
+#include "timer.h"
+#include "video.h"
+#include "mouse.h"
+
+typedef struct {
+	void (*printView)();
+
+	int (*getYear)();
+	void (*setYear)();
+	int (*getMonth)();
+	void (*setMonth)();
+	int (*getWeek)();
+	void (*setWeek)();
+
+	int year, month, week;
+} View;
+
+static void setYear(View *this, int year) {
+    this->year = year;
+}
+
+static void setMonth(View *this, int month) {
+    this->month = month;
+}
+
+static void setWeek(View *this, int week) {
+    this->week = week;
+}
+
+static int getYear(View *this) {
+    return this->year;
+}
+
+static int getMonth(View *this) {
+    return this->month;
+}
+
+static int getWeek(View *this) {
+    return this->week;
+}
+
+#endif
