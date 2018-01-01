c4d2aca4fb48b35d62903ce96b89ae378dbd5c7d
Daniel Silva <danny@dannyps.net>
2018-01-01 23:33:19 +0000

    Continue experimenting with doxygen

--- Proj/Src/e_cal.h
+++ Proj/Src/e_cal.h
@@ -3,6 +3,11 @@
 
 /**
  * @mainpage eCal
+ * @section intro_sec Introduction
+ * This is an intro
+ * \section install_sec Installation
+ *
+ * \subsection step1 Step 1: Opening the box
  */
 
 /**
