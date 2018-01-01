e4f4156856a3e7a514a3d18d6230188219b1bdc1
up201503212 <up201503212@0416bdf5-df4b-41b9-a32b-9c14e5908e50>
2018-01-01 22:44:59 +0000

    Continue experimenting with doxygen

--- Proj/Src/view.h
+++ Proj/Src/view.h
@@ -2,19 +2,35 @@
 #define __VIEW_H
 #include "read_xpm.h"
 
+
+/** @defgroup view view
+ * @{
+ *
+ * @brief structs and functions to handle the calendar generation.
+ */
+
+/**
+ * @brief relevant details to a view
+ */
 typedef struct view{
-	int year;
-	int month;
-	int daysInTheMonth;
-	xpm_t* xpm;
+	int year; 			/**< @brief current year*/
+	int month;			/**< @brief current month*/
+	int daysInTheMonth;	/**< @brief days in the current month/year pair*/
+	xpm_t* xpm;			/**< @brief pointer to the xpm associated with the current month name*/
 } View;
 
-
+/**
+ * @brief relevant XPMs
+ */
 extern xpm_t XPM_exitCross, XPM_january, XPM_february, XPM_march, XPM_april, XPM_may, XPM_june; // camelCase please!
 extern xpm_t XPM_july, XPM_august, XPM_september, XPM_october, XPM_november, XPM_december;
 extern xpm_t XPM_monday, XPM_tuesday, XPM_wednesday, XPM_thursday, XPM_friday, XPM_saturday, XPM_sunday;
 
-
+/**
+ * @brief advance 1 year
+ * @param pointer to the View
+ * @return void
+ */
 void nextMonth(View *v);
 void prevMonth(View *v);
 
