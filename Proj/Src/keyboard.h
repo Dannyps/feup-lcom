0581b6a806c1ee2de524a344936eb78ff7cd89d2
Daniel Silva <danny@dannyps.net>
2018-01-01 22:21:44 +0000

    Continue experimenting with doxygen

--- Proj/Src/keyboard.h
+++ Proj/Src/keyboard.h
@@ -1,10 +1,16 @@
 #ifndef __KEYBOARD_H
 #define __KEYBOARD_H
 
+/**
+ * @brief Struct to manage a received interrupt from the keyboard.
+ *
+ * @note mk is always the compliment of bk.
+ */
 typedef struct key_press_t{
-	unsigned char code; /**< @brief the code generated, without 0xe0 prefix */
-	unsigned char is2Byte; /**< @brief wether the 0xe0 byte was present */
-	unsigned char mk, bk; /**< @brief is makecode/breakcode */
+	unsigned char code; /**< @brief the code generated, without the 0xe0 prefix */
+	unsigned char is2Byte; /**< @brief whether the 0xe0 byte was present */
+	unsigned char mk; /**< @brief 1 if is makecode, 0 otherwise */
+	unsigned char bk; /**< @brief 1 if is breakcode, 0 otherwise */
 } KEY_PRESS;
 
 int kbd_subscribe_int(void );
