27ab2b2eeab0db4872b52159c7d069ffc15b7b84
up201503212 <up201503212@0416bdf5-df4b-41b9-a32b-9c14e5908e50>
2018-01-01 23:58:10 +0000

    Continue experimenting with doxygen

--- Proj/Src/e_cal.h
+++ Proj/Src/e_cal.h
@@ -4,10 +4,24 @@
 /**
  * @mainpage eCal
  * @section intro_sec Introduction
- * This is an intro
- * \section install_sec Installation
+ * eCal is an application developed in the context of <a href="https://sigarra.up.pt/feup/pt/ucurr_geral.ficha_uc_view?pv_ocorrencia_id=399888">LCOM</a>.
+ * It depends heavily in device drivers developed specifically for this use.
  *
- * \subsection step1 Step 1: Opening the box
+ * Authors:
+ *  - Daniel Silva 		daniel.s@fe.up.pt
+ *  - Frederico Rocha
+ *
+ * \section how2run How to run eCal
+ * \subsection step1 Step 1: Creating the Symlink
+ * Because of the way this program is loaded into memory, all paths must be absolute.
+ * In order to facilitate reading auxiliary files, we require the creation of a soft link.
+ * You can do this by running: <code>ln -s /path/to/your/working/copy /pr</code>
+ * \subsection step2 Step 2: Creating the configuration file
+ * You must copy the contents of the file <code>Conf/proj</code> to the directory <code>/etc/system.conf.d/</code>
+ * \subsection step3 Step 3: Building the application
+ * Change directory to <code>/pr/Src</code> and run <code>make</code>.
+ * \subsection step4 Step 4: Running the service
+ * Run the command <code>service run /pr/Src/proj -args init</code>, as root, in order to initialize the service.
  */
 
 /**
