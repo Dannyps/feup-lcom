29b65442f92199b5442ad348a9e9aa0111454041
Daniel Silva <danny@dannyps.net>
2018-01-01 13:01:19 +0000

    bcd decode

--- lab6/test6.h
+++ lab6/test6.h
@@ -2,3 +2,13 @@ int rtc_test_conf(void);
 int rtc_test_date(void);
 int rtc_test_int();
 int rtc_test_settime(char* s1, char* s2);
+
+typedef struct {
+	unsigned char hour;
+	unsigned char min;
+	unsigned char sec;
+	unsigned char day;
+	unsigned char month;
+	unsigned char year;
+	unsigned char wd;
+} rtc_time_t;
