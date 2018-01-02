8d86ec1d50c842c2479e6cd61c3b6dd9cf9fc8a4
up201503212 <up201503212@0416bdf5-df4b-41b9-a32b-9c14e5908e50>
2018-01-02 00:54:38 +0000

    Continue experimenting with doxygen

--- Proj/Src/read_xpm.h
+++ Proj/Src/read_xpm.h
@@ -16,7 +16,23 @@ typedef struct xpm{
 	char success;  /**< @brief Whether the xpm was successfully read.*/
 } xpm_t;
 
+/**
+ * @brief read xpm from char**
+ * @warning Returned memory should be freed asap!
+ * @warning This function requires map to be defined in the object file, thus making the linked file too big to run. Please use read_xpm_from_file(char* filename)!
+ * @param map pointer to array
+ * @param wd xpm width returned here
+ * @param ht xpm height returned here
+ * @return pointer to mem position that holds the video data.
+ */
 char* read_xpm(char *map[], int *wd, int *ht);
+
+/**
+ * @brief read xpm from file filename
+ * @warning Returned memory should be freed asap! And then again, probably shouldn't because you'll need this until the end.
+ * @param filename absolute path to file
+ * @return xpm_t with a pointer to the video data.
+ */
 xpm_t read_xpm_from_file(char* filename);
 
 /**}*/
